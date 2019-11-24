e3ef310da87c3c13817e7835b624165ed3c8941e
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-11-24 07:56:13 +0700

    range

---
+++ double_point.rb
@@ -0,0 +1,6 @@
+r = 1..5
+puts "Nilai dari R  adalah #{r} dengan tipe #{r.class}"
+
+for i in r
+	puts "this is i in r => #{i}"
+end
---
+++ member.rb
@@ -0,0 +1,5 @@
+r = 1...5
+puts "Nilai dari R  adalah #{r} dengan tipe #{r.class}"
+puts "apakah 6 bagian dari R? #{r.member?(6)}"
+puts "apakah 4 termasuk dari R? #{r.include?(4)}"
+
